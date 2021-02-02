f8eb401970fa5cc39b19bc62a9af262e90a5f000
Emery Kouassi Assogba <assogba.emery@gmail.com>
2021-02-02 12:18:07 +0100

    update to figure

--- topo.py
+++ topo.py
@@ -1,37 +1,13 @@
 #!/usr/bin/python
 
-"""
-linuxrouter.py: Example network with Linux IP router
-
-This example converts a Node into a router using IP forwarding
-already built into Linux.
-
-The example topology creates a router and three IP subnets:
-
-    - 192.168.1.0/24 (r0-eth1, IP: 192.168.1.1)
-    - 172.16.0.0/12 (r0-eth2, IP: 172.16.0.1)
-    - 10.0.0.0/8 (r0-eth3, IP: 10.0.0.1)
-
-Each subnet consists of a single host connected to
-a single switch:
-
-    r0-eth1 - s1-eth1 - h1-eth0 (IP: 192.168.1.100)
-    r0-eth2 - s2-eth1 - h2-eth0 (IP: 172.16.0.100)
-    r0-eth3 - s3-eth1 - h3-eth0 (IP: 10.0.0.100)
-    
-                  h2---s2----r0---s1--h1
-                             |
-                             |
-                             s3
+"""     
+                        h2
+                        |
+                        |
+                  h1---r0---r1-----h4
                              |
                              |
                              h3
-The example relies on default routing entries that are
-automatically created for each router interface, as well
-as 'defaultRoute' parameters for the host interfaces.
-
-Additional routes may be added to the router or hosts by
-executing 'ip route' or 'route' commands on the router or hosts.
 """
 
 
